62f5ba45551f7842800cfcbc7fd30fe3a25cfe34
Sog1n <siuace492@gmail.com>
2025-10-26 23:29:33 +0700

    update cors

--- backend/index.js
+++ backend/index.js
@@ -19,10 +19,20 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use(cors({
+
+app.use(
+    cors({
+        origin: "https://cicd-ashen.vercel.app",
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
+app.options("*", cors({
     origin: "https://cicd-ashen.vercel.app",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
 
